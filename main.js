ffa3b78c13b136ffd6eae2a83636a75b116357f1
Angie Staffieri <angie.staffieri@gmail.com>
2022-09-24 16:27:49 -0600

    Show the appropriate game pieces depending on which type of game is chosen

--- main.js
+++ main.js
@@ -1,52 +1,29 @@
-var newGame = new Game("human", "computer")
-var newGame1 = new Game("human", "computer")
-var newGame2 = new Game("human", "computer")
-var newGame3 = new Game("human", "computer")
+var newGame = new Game()
 
+//querySelectors
+var chooseClassic = document.querySelector('.classic-section')
+var chooseWeaponText = document.querySelector('.choose-weapon')
+var showClassicPieces = document.querySelector('.game-pieces')
+var chooseComplex = document.querySelector('.complex-section')
+var showComplexPieces = document.querySelector('.complex-pieces')
 
-// function weaponPicker() {
-//   console.log("Pick your game! - simple or complex")
-//   newGame.human.takeTurn()
-//
-// }
-//
-// weaponPicker()
+//eventListeners
+chooseClassic.addEventListener('click', showClassic)
+chooseComplex.addEventListener('click', showComplex)
 
-// Player gets two choices - simple game or complex game. The player picks one.
-// The player picks a token.
-// One of them wins. Winner is displayed.
-newGame.pickGame("simple")
-newGame.pickToken("rock")
-console.log()
-console.log(newGame.checkWinner())
-console.log(newGame.human.wins)
-console.log(newGame.computer.wins)
-newGame1.pickGame("complex")
-newGame1.pickToken("rock")
-console.log()
-console.log(newGame1.checkWinner())
-console.log(newGame1.human.wins)
-console.log(newGame1.computer.wins)
-newGame.pickGame("simple")
-newGame.pickToken("rock")
-console.log()
-console.log(newGame.checkWinner())
-console.log(newGame.human.wins)
-console.log(newGame.computer.wins)
+//functions
+function showClassic() {
+  chooseClassic.classList.add('hidden')
+  chooseComplex.classList.add('hidden')
+  chooseWeaponText.classList.remove('hidden')
+  showClassicPieces.classList.remove('hidden')
+}
 
-newGame2.pickGame("complex")
-newGame2.pickToken("scissors")
-console.log()
-console.log(newGame2.checkWinner())
-console.log(newGame2.human.wins)
-console.log(newGame2.computer.wins)
+function showComplex() {
+  chooseClassic.classList.add('hidden')
+  chooseComplex.classList.add('hidden')
+  chooseWeaponText.classList.remove('hidden')
+  showClassicPieces.classList.remove('hidden')
+  showComplexPieces.classList.remove('hidden')
 
-newGame3.pickGame("simple")
-newGame3.pickToken("paper")
-console.log()
-console.log(newGame3.checkWinner())
-console.log(newGame3.human.wins)
-console.log(newGame3.computer.wins)
-
-
-//
+}
